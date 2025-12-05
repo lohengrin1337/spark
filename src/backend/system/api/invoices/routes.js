eb526bd32ed43345fff2f24cc903e0916484c848
Emma <emmawidengrd@yahoo.se>
2025-12-05 12:13:29 +0100

    Add comments and invoice by id endpoint

--- src/backend/system/api/invoices/routes.js
+++ src/backend/system/api/invoices/routes.js
@@ -1,9 +1,22 @@
 // invoice endpoints
-
+// each endpoint listed as a route with a method and
+// a controller function
 const router = require('express').Router();
 const invoiceController = require('./controller');
 
+/**
+ * GET invoices
+ * Returns an array of all invoices in the database.
+ * Response: 200 ok and array of invoice objects.
+ */
 router.get('/', invoiceController.getInvoices);
+/**
+ * GET /:id
+ * Returns invoice that matches query param :id
+ * @param id Invoice id
+ * Response: 200 ok and invoice object or 404 not found.
+ */
+router.get('/:id', invoiceController.getInvoiceById);
 
 module.exports = router;
 

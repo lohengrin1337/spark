b9e85a20c80c3b84ce04ca343800ee1d6d08a6aa
Emma <emmawidengrd@yahoo.se>
2025-12-09 18:21:58 +0100

    Restructure api folder for clarity and separation

---
+++ src/backend/system/api/v1/cities/cities.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require('express').Router();
+
+/**
+ * GET cities
+ * Response: 200 ok and array of cities objects.
+ */
+router.get('/', async (req, res) => {
+
+});
+
+
+module.exports = router;

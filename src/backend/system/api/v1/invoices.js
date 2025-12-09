788291ba7f258c36b4f264190a15ac33d3434b87
Emma <emmawidengrd@yahoo.se>
2025-12-09 09:41:44 +0100

    Wip

--- src/backend/system/api/v1/invoices.js
+++ src/backend/system/api/v1/invoices.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const invoiceServices = require('../../services/invoiceServices');
 const router = require('express').Router();
+const validate = require('../middleware/validate.js');
+const authenticate = require('../middleware/authenticate.js');
+const authorize = require('../middleware/authorize.js');
+const invoiceServices = require('../../services/invoiceServices');
 
 router.use(auth.user());
 
@@ -9,25 +11,24 @@ router.use(auth.user());
  * GET invoices
  * Response: 200 ok and array of invoice objects.
  */
-router.get('/', async (req, res) => {
-    const invoiceId = req.params.id;
-    try {
-        const invoice = await invoiceServices.getInvoiceById(invoiceId);
-        if (!invoice) {
-            return res.status(404).json({ error: 'Invoice not found'});
-        }
-        res.status(200).json(invoice);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to fetch.'});
-    }
+router.get('/',
+    authenticate, // koll valid token
+    validate, // koll valid request
+    authorize, // k
+    async (req, res) => {
+    
 });
 
 /**
  * GET /:id
  * Response: 200 ok and invoice object or 404 not found.
  */
-router.get('/:id', async (req, res) => {
-    const invoiceId = req.params.id;
+router.get('/:id',
+    authenticate, //kollar att det finns en valid token, avkodar, fäster info på req.user
+    validate, //validerar requesten
+    authorizeInvoiceAccess, // kollar om fakturan får hämtas (jämför user id)
+    async (req, res) => {
+    const invoiceId = req.params;
     try {
         const invoice = await invoiceServices.getInvoiceById(invoiceId);
         if (!invoice) {

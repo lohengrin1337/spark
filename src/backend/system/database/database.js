01c89bdf776b0962da824513a7c986f7c1c4dbef
h3rmano <h3rmanoo@gmail.com>
2025-12-17 04:45:04 +0100

    Use .env-based variables also in database.js

--- src/backend/system/database/database.js
+++ src/backend/system/database/database.js
@@ -3,11 +3,11 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: 'mariadb',
-  user: 'root',
-  password: 'admin',
-  database: 'spark_db',
-  connectionLimit: 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
 });
 
 module.exports = pool;

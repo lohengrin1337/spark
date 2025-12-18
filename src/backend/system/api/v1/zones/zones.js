1061e6a1bd6eea77b26782ab0882146fa4a09e24
Emma <emmawidengrd@yahoo.se>
2025-12-18 12:12:08 +0100

    Forgot to please SonarCloud

--- src/backend/system/api/v1/zones/zones.js
+++ src/backend/system/api/v1/zones/zones.js
@@ -20,23 +20,4 @@ router.get('/',
     res.status(200).json(zones);
 });
 
-/**
- * GET /:id
- * Response: 200 ok and zone object or 404 not found.
- */
-// router.get('/:type',
-//     async (req, res) => {
-//     const zoneType = String(req.params.type);
-//     console.log(zoneType);
-//     try {
-//         const zone = await zoneService.getZoneByType(zoneType);
-//         if (!zone) {
-//             return res.status(404).json({ error: 'Zone not found'});
-//         }
-//         res.status(200).json(zone);
-//     } catch (err) {
-//         res.status(500).json({ error: 'Failed to fetch.'});
-//     }  
-// });
-
 module.exports = router;

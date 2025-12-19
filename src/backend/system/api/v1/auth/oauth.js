4d23ca64d49323bfa84951d86dab62a4bf52b27a
Olof Jönsson <epost.olof@gmail.com>
2025-12-19 09:17:54 +0100

    Add code to return user to frontend

--- src/backend/system/api/v1/auth/oauth.js
+++ src/backend/system/api/v1/auth/oauth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const authService = require('./authService');
+const authService = require('./authService');
 
 /**
  * This endpoint is called from frontend when using GitHub to signup/login,
@@ -14,14 +14,15 @@ router.get("/github", (req, res) => {
 });
 
 /**
- * This endpoint is called by github, and is used to get user data
+ * This endpoint is called by github to deliver a code.
+ * The code is then exchanged for a token,
+ * which is used to fetch user data
  */
 router.get("/github/callback", async (req, res) => {
     // Code from github
     const code = req.query.code;
 
     console.log("CODE FROM GITHUB:", code);
-
 
     // Exchange code for token
     const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
@@ -37,8 +38,7 @@ router.get("/github/callback", async (req, res) => {
 
     console.log("TOKEN FROM GITHUB:", access_token);
 
-
-    // Exchange token for user
+    // Fetch user profile with token
     const userRes = await fetch("https://api.github.com/user", {
         headers: { Authorization: `Bearer ${access_token}` }
     });
@@ -46,6 +46,7 @@ router.get("/github/callback", async (req, res) => {
 
     console.log("USER FROM GITHUB:", user);
 
+    // Example of some real user data
     // {
     //     login: 'lohengrin1337',
     //     id: 149483141,
@@ -54,21 +55,29 @@ router.get("/github/callback", async (req, res) => {
     //     email: null,
     // }
 
+    // Fetch email addresses with token
     const emailsRes = await fetch('https://api.github.com/user/emails', {
         headers: { Authorization: `Bearer ${access_token}` }
     });
     const emails = await emailsRes.json();
     console.log("emails FROM GITHUB:", emails);
 
+    // Get the primary email address
     const primaryEmail = emails.find(e => e.primary)?.email;
     console.log("primaryEmail FROM GITHUB:", primaryEmail);
-
-    const userData = {
-        provider: "github",
-        provider_id: user.id,
-        email: primaryEmail || null
+    
+    // Prepare customer for database
+    const customer = {
+        name: user.name || null,
+        email: primaryEmail || null,
+        oauth_provider: "github",
+        oauth_provider_id: user.id,
     };
     
+    console.log("CUSTOMER:", customer);
+
+    // TOKEN SHOULD BE RETURNED HERE LATER
+    res.json({ customer });
 });
 
 module.exports = router;

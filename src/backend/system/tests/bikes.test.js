77c5ac10466e0dee630ca5e9c4d68da32394acda
Emma <emmawidengrd@yahoo.se>
2025-12-21 10:51:14 +0100

    Add: tests for bikes and customers endpoints

--- src/backend/system/tests/bikes.test.js
+++ src/backend/system/tests/bikes.test.js
@@ -1,16 +1,17 @@
+// Tests for bikes endpoint.
 const request = require('supertest');
 const app = require('../app');
 
 describe('bikes endpoint', () => {
 
-    it('should get all bikes', async () => {
+    it('GET /bikes', async () => {
         const res = await request(app).get('/api/v1/bikes');
         expect(res.statusCode).toEqual(200);
         expect(Array.isArray(res.body)).toBeTruthy();
     });
 });
 
-describe('filter on status', () => {
+describe('GET /bikes, filter on status', () => {
     it('should return all available bikes', async () => {
         const res = await request(app).get('/api/v1/bikes?status=available');
         expect(res.statusCode).toEqual(200);
@@ -18,10 +19,17 @@ describe('filter on status', () => {
     })
 });
 
-describe('get a bike filtered on id', () => {
+describe('GET /bikes, filter on bike_id', () => {
     it('should return bike with bike_id 5', async () => {
         const res = await request(app).get('/api/v1/bikes/5');
         expect(res.statusCode).toEqual(200);
         expect(res.body.bike_id).toEqual(5);
     })
-})
+});
+
+describe('GET /bikes, invalid id', () => {
+    it('should throw a 404 not found', async () => {
+        const res = await request(app).get('/api/v1/bikes/abc');
+        expect(res.statusCode).toEqual(404);
+    })
+});
--- src/backend/system/tests/customers.test.js
+++ src/backend/system/tests/customers.test.js
@@ -1,11 +1,26 @@
+//Tests for customers endpoint
 const request = require('supertest');
 const app = require('../app');
 
-describe('customers endpoint', () => {
-
+describe('GET /customers', () => {
     it('should get all customers', async () => {
         const res = await request(app).get('/api/v1/customers');
         expect(res.statusCode).toEqual(200);
         expect(Array.isArray(res.body)).toBeTruthy();
     });
+});
+
+describe('GET /customers/:id', () => {
+    it('should return costumer with id 4', async () => {
+        const res = await request(app).get('/api/v1/customers/4');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.customer_id).toEqual(4);
+    });
+});
+
+describe('GET /customers, invalid id', () => {
+    it('should throw a 404 not found', async () => {
+        const res = await request(app).get('/api/v1/customers/abc');
+        expect(res.statusCode).toEqual(404);
+    })
 });

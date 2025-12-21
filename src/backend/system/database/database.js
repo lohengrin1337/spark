ea605aed21d26b863e8a1819aa4487f447ed7167
Emma <emmawidengrd@yahoo.se>
2025-12-21 17:41:09 +0100

    Add: mariadb image as a service in the workflow file, ENV variables in the database connection file

--- src/backend/system/database/database.js
+++ src/backend/system/database/database.js
@@ -3,10 +3,10 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: 'mariadb', 
-  user: 'root',
-  password: 'admin',
-  database: 'spark_db',
+  host: process.env.DB_HOST || 'mariadb', 
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'admin',
+  database: process.env.DB_NAME || 'spark_db',
   connectionLimit: 10,
 });
 

65f2d0ca556edd3d9dec51dd15a99383540b91fc
Emma <emmawidengrd@yahoo.se>
2026-01-02 20:05:30 +0100

    Add: checks token and role, redirects to login form if not admin

--- src/frontend/shared/auth/admin-check.js
+++ src/frontend/shared/auth/admin-check.js
@@ -1,5 +1,8 @@
 // Checks if admin has token in session, otherwise redirects to the admin-login-page.
-
-if (!localStorage.getItem('adminToken')) {
+const token = localStorage.getItem("token");
+console.log(token);
+if (!token) {
     window.location.replace('/admin-login.html');
   }
+const { role } = JSON.parse(atob(token.split(".")[1]));
+if (role !== "admin") location.href = "/403.html";

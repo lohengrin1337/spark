8453fd6ba9c038fec897bab74649d1fe877119a1
Emma <emmawidengrd@yahoo.se>
2026-01-03 10:30:15 +0100

    Fix: admin check hickup

--- src/frontend/shared/auth/admin-check.js
+++ src/frontend/shared/auth/admin-check.js
@@ -1,14 +1,10 @@
 // Checks if admin has token in session, otherwise redirects to the admin-login-page.
 const token = localStorage.getItem("token");
 console.log(token);
+if (!token) {
+    window.location.replace('/admin-login.html');
+}
+
 const { role } = JSON.parse(atob(token.split(".")[1]));
 console.log("ROLE", role);
-function checkTokenRole(token) {
-    if (!token) {
-        window.location.replace('/admin-login.html');
-        console.log("AAAAAA")
-        return;
-      }
-
-}
 if (role !== "admin") location.href = "/403.html";

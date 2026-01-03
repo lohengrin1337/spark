5c2fcd9ce62d97e4c9f3cfe6c1aef0a25a6f029a
Olof Jönsson <epost.olof@gmail.com>
2026-01-03 11:39:11 +0100

    Update zoneModel to show bikes in each zone
    * Each zone includes an array of bike ids
        * `{..., bikes: [1,2,3] }`
    * Replace `getAllZones` and `getFilteredZones` with `getZones`,
    which takes an optional `filter` parameter

--- src/backend/system/api/v1/zones/zoneModel.js
+++ src/backend/system/api/v1/zones/zoneModel.js
@@ -2,29 +2,71 @@
 
 const pool = require('../../../database/database');
 
+
 const zoneModel = {
-  /**
-   * Fetch all zones in database
-   * @returns { Array } Array of zone objects.
-   * @throws { Error } If the query fails.
-   */
-  async getAllZones() {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        const zones = await conn.query(
-            `SELECT 
-            zone_id,
-            city,
-            zone_type,
-            ST_AsText(coordinates) AS coordinates
-            FROM spark_zone
-            ORDER BY city ASC, zone_id ASC`);
-        return zones;
-    } finally {
-        if (conn) conn.release();
-    }
-  },
+
+    // Select id, city, type, coords and bikes for each zone
+    sharedQuery: `
+        SELECT 
+            z.zone_id,
+            z.city,
+            z.zone_type,
+            ST_AsText(z.coordinates) AS coordinates,
+            JSON_ARRAYAGG(DISTINCT b.bike_id) AS bikes
+        FROM spark_zone AS z
+        LEFT JOIN bike AS b
+        ON ST_Contains(z.coordinates, b.coordinates)
+    `,
+
+//   /**
+//    * Fetch all zones and include bikes (id) contained in each zone
+//    * @returns { Array } Array of zone objects.
+//    * @throws { Error } If the query fails.
+//    */
+//   async getAllZones() {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const zones = await conn.query(
+//             this.sharedQuery + `
+//             GROUP BY z.zone_id
+//             ORDER BY city ASC, zone_id ASC`
+//         );
+
+//         // Remove null values from bikes array [null] -> []
+//         zones.forEach(z => {
+//             z.bikes = z.bikes.filter(Number.isInteger);
+//         });
+
+//         return zones;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+//   },
+
+//   /**
+//    * Fetch all zones in database
+//    * @returns { Array } Array of zone objects.
+//    * @throws { Error } If the query fails.
+//    */
+//   async getAllZones() {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const zones = await conn.query(
+//             `SELECT 
+//             zone_id,
+//             city,
+//             zone_type,
+//             ST_AsText(coordinates) AS coordinates
+//             FROM spark_zone
+//             ORDER BY city ASC, zone_id ASC`);
+//         return zones;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+//   },
+  
   /**
    * Fetch one zone, filter on zone_id.
    * @returns { object } Zone data as object.
@@ -33,41 +75,158 @@ const zoneModel = {
     let conn;
     try {
         conn = await pool.getConnection();
-        const zone = await conn.query("SELECT * FROM spark_zone WHERE zone_id = ?", [id]);
+
+        const zone = await conn.query(
+            this.sharedQuery + `
+            WHERE z.zone_id = ?
+            GROUP BY z.zone_id
+            ORDER BY city ASC, zone_id ASC`,
+            [id]
+        );
+
         return zone[0];
     } finally {
         if (conn) conn.release();
     }
   },
+  
+//   /**
+//    * Fetch one zone, filter on zone_id.
+//    * @returns { object } Zone data as object.
+//    */
+//   async getZoneById(id) {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const zone = await conn.query("SELECT * FROM spark_zone WHERE zone_id = ?", [id]);
+//         return zone[0];
+//     } finally {
+//         if (conn) conn.release();
+//     }
+//   },
 
   /**
-   * Fetch zones filtered on zone type
-   * @param { object } filter may contain city and/or zone_type
+   * Fetch zones and include bikes (id) contained in each zone
+   * Optional filter on city and type
+   * @param { object } filter may contain city and/or type
    * @returns { Array } array of zones that match
    */
-  async getFilteredZones(filter) {
-    let conn;
-    let query = "SELECT * FROM spark_zone WHERE ";
+  async getZones(filter) {            
+    let query = this.sharedQuery;
+
     const params = [];
-    if (filter.city) {
-        query += "city = ?";
-        params.push(filter.city);
+    if (filter.city || filter.type) {
+        query += " WHERE ";
+        
+        if (filter.city) {
+            query += "z.city = ?";
+            params.push(filter.city);
+        }
+        if (filter.city && filter.type) {
+            query += " AND ";
+        }
+        if (filter.type) {
+            query += "z.zone_type = ?";
+            params.push(filter.type);
+        }
     }
-    if (filter.city && filter.type) {
-        query += " AND ";
-    }
-    if (filter.type) {
-        query += "zone_type = ?";
-        params.push(filter.type);
-    }
+
+    query += `
+        GROUP BY z.zone_id
+        ORDER BY city ASC, zone_id ASC`;
+
+    console.log(query);
+    
+
+    let conn;
     try {
         conn = await pool.getConnection();
-        const zones = await conn.query(`${query};`, params);
+        const zones = await conn.query(query, params);
+
+        // Remove null values from bikes array [null] -> []
+        zones.forEach(z => {
+            z.bikes = z.bikes.filter(Number.isInteger);
+        });
+
         return zones;
     } finally {
         if (conn) conn.release();
     }
   }
+
+//   /**
+//    * Fetch zones filtered on zone type
+//    * @param { object } filter may contain city and/or zone_type
+//    * @returns { Array } array of zones that match
+//    */
+//   async getFilteredZones(filter) {            
+//     let query = this.sharedQuery;
+
+//     const params = [];
+//     if (filter) {
+//         query += " WHERE ";
+        
+//         if (filter.city) {
+//             query += "z.city = ?";
+//             params.push(filter.city);
+//         }
+//         if (filter.city && filter.type) {
+//             query += " AND ";
+//         }
+//         if (filter.type) {
+//             query += "z.zone_type = ?";
+//             params.push(filter.type);
+//         }
+//     }
+
+//     query += `
+//         GROUP BY z.zone_id
+//         ORDER BY city ASC, zone_id ASC`;
+
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const zones = await conn.query(query, params);
+
+//         // Remove null values from bikes array [null] -> []
+//         zones.forEach(z => {
+//             z.bikes = z.bikes.filter(Number.isInteger);
+//         });
+
+//         return zones;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+//   }
+
+//   /**
+//    * Fetch zones filtered on zone type
+//    * @param { object } filter may contain city and/or zone_type
+//    * @returns { Array } array of zones that match
+//    */
+//   async getFilteredZones(filter) {
+//     let conn;
+//     let query = "SELECT * FROM spark_zone WHERE ";
+//     const params = [];
+//     if (filter.city) {
+//         query += "city = ?";
+//         params.push(filter.city);
+//     }
+//     if (filter.city && filter.type) {
+//         query += " AND ";
+//     }
+//     if (filter.type) {
+//         query += "zone_type = ?";
+//         params.push(filter.type);
+//     }
+//     try {
+//         conn = await pool.getConnection();
+//         const zones = await conn.query(`${query};`, params);
+//         return zones;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+//   }
 };
 
 module.exports = zoneModel;
--- src/backend/system/api/v1/zones/zoneService.js
+++ src/backend/system/api/v1/zones/zoneService.js
@@ -3,17 +3,27 @@
 const zoneModel = require('./zoneModel');
 
 /**
- * Gets all zones from model if no filter
- * or filter on city and/or zone type.
+ * Gets matching zones from model
+ * Filters are optional
  * @returns Array of zones
- * @param { object } filter
+ * @param { object } filter { city, type }
  */
 async function getZones(filter) {
-    if (!filter.city && !filter.type) {
-        return zoneModel.getAllZones();
-    }
-    return zoneModel.getFilteredZones(filter);
+    return zoneModel.getZones(filter);
 };
+
+// /**
+//  * Gets all zones from model if no filter
+//  * or filter on city and/or zone type.
+//  * @returns Array of zones
+//  * @param { object } filter
+//  */
+// async function getZones(filter) {
+//     if (!filter.city && !filter.type) {
+//         return zoneModel.getAllZones();
+//     }
+//     return zoneModel.getFilteredZones(filter);
+// };
 
 
 /**

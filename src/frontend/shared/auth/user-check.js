8975e485b6af573605a30e09fb580dc4f31906a8
Emma <emmawidengrd@yahoo.se>
2026-01-03 01:08:00 +0100

    Add: extend user check

--- src/frontend/shared/auth/user-check.js
+++ src/frontend/shared/auth/user-check.js
@@ -3,3 +3,17 @@
 if (!localStorage.getItem('token')) {
     window.location.replace('/user-login.html');
   }
+// Checks if admin has token in session, otherwise redirects to the admin-login-page.
+const token = localStorage.getItem("token");
+console.log(token);
+const { role } = JSON.parse(atob(token.split(".")[1]));
+console.log("ROLE", role);
+function checkTokenRole(token) {
+    if (!token) {
+        window.location.replace('/user-login.html');
+        console.log("AAAAAA")
+        return;
+      }
+
+}
+if (role !== "customer") location.href = "/403.html";

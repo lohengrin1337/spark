78b19e5371a96d66ba116673801c0fb26c4b635c
Olof Jönsson <epost.olof@gmail.com>
2026-01-05 13:01:09 +0100

    Fix admin and user token checks
    * add support to check for expired token

--- src/frontend/shared/auth/admin-check.js
+++ src/frontend/shared/auth/admin-check.js
@@ -1,10 +1,29 @@
 // Checks if admin has token in session, otherwise redirects to the admin-login-page.
+// Also check if token has the right role and is not expired
+
 const token = localStorage.getItem("token");
 console.log(token);
+
 if (!token) {
     window.location.replace('/admin-login.html');
+} else {
+    try {
+        // Parse base-64 encoded jwt token
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const { role, exp } = payload;
+        const now = Math.floor(Date.now() / 1000);
+    
+        console.log("ROLE", role);
+        console.log("EXP", exp);
+        console.log("NOW", now);
+    
+        // Check role & expire date
+        if (role !== "customer" || now > exp) {
+            localStorage.removeItem('token');
+            window.location.replace('/user-login.html');
+        }
+        } catch (err) {
+        localStorage.removeItem('token');
+        window.location.replace('/user-login.html');
+    }
 }
-
-const { role } = JSON.parse(atob(token.split(".")[1]));
-console.log("ROLE", role);
-if (role !== "admin") location.href = "/403.html";
--- src/frontend/shared/auth/user-check.js
+++ src/frontend/shared/auth/user-check.js
@@ -1,29 +1,37 @@
 // Checks if user has token in session or as query parameter, otherwise redirects to the user-login-page.
+// Also check if token has the right role and is not expired
+
 let token = localStorage.getItem('token');
 
 if (!token) {
+  // Check for token in url query 
   const searchParams = new URLSearchParams(window.location.search);
   token = searchParams.get("token");
-
+  
   if (!token) {
     window.location.replace('/user-login.html');
+  } else { 
+    localStorage.setItem('token', token);
+    window.history.replaceState({}, "", window.location.pathname);
   }
-
-  localStorage.setItem('token', token);
-  window.history.replaceState({}, "", window.location.pathname);
-}
-
-try {
-  const payload = JSON.parse(atob(token.split('.')[1]));
-  const { role } = payload;
-
-  console.log("ROLE", role);
-
-  if (role !== "customer") {
-    window.location.replace("/403.html");
+} else {
+  try {
+    // Parse base-64 encoded jwt token
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const { role, exp } = payload;
+    const now = Math.floor(Date.now() / 1000);
+    
+    console.log("ROLE", role);
+    console.log("EXP", exp);
+    console.log("NOW", now);
+    
+    // Check role & expire date
+    if (role !== "customer" || now > exp) {
+      localStorage.removeItem('token');
+      window.location.replace('/user-login.html');
+    }
+  } catch (err) {
+    localStorage.removeItem('token');
+    window.location.replace('/user-login.html');
   }
-} catch (err) {
-  console.error("Invalid token", err);
-  localStorage.removeItem('token');
-  window.location.replace('/user-login.html');
 }

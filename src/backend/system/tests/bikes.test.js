bb6560b84808b5c5d939e139a1196d810c4fad92
kh3rm <h3rmanoo@gmail.com>
2026-01-16 09:34:34 +0100

    Retire workflow for the finished project

--- src/backend/system/tests/bikes.test.js
+++ src/backend/system/tests/bikes.test.js
@@ -12,17 +12,9 @@ describe('bikes endpoint', () => {
     });
 });
 
-describe('GET /bikes, filter on status', () => {
-    it('should return all available bikes', async () => {
-        const res = await request(app).get('/api/v1/bikes?status=available');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body[0].status).toEqual('available');
-    });
-});
-
 describe('GET /bikes, filter on bike_id', () => {
     it('should return bike with bike_id 5', async () => {
-        const res = await request(app).get('/api/v1/bikes/5');
+        const res = await request(app).get('/api/v1/bikes?bike_id=5');
         expect(res.statusCode).toEqual(200);
         expect(res.body.bike_id).toEqual(5);
     });
